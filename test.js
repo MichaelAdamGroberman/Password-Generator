152a1a14829506e288fbe0ef9781549606608e18
Michael Groberman <michaelgroberman@MacBook-Pro.local>
2021-06-23 22:01:55 -0400

    finalized js, css, and html
    created html document and elements that are referenced in the js script. I updated the CSS to add styling that mimics the design in the assignment's readme file. In the js script, I defined variables and constants to store all character category types, and all user prompt inputs. I added a function to generate a random password, and a function to copy the password to your clipboard by pressing a button on the page

--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-// Define all characters to use in the generated password
-var pwdChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$%&'()*+,-./:;<=>?@^_`{|}~";
-
-// Define number of characters in the generated password
-var pwdLength = 10;
-
-// Define the value of the generated password using a map function to loop through the available password characters, selecting a random position in the array to add to the password.
-var generatedPassword = Array(pwdLength).fill(pwdChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
-
-// Print the generatedPassword in the console.log
-console.log(generatedPassword);
+var pwdChars = “0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$%&‘()*+,-./:;<=>?@^_`{|}~“;
+var pwdLen = 10;
+var randPassword = Array(pwdLen).fill(pwdChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join(‘’);
